80cc79f66764efda919182a94b649405610e0ef8
Craig Helmann <chelmann@psrc.org>
2023-12-12 10:22:02 -0800

    updated app deployment script

--- deploy.R
+++ deploy.R
@@ -1,13 +1,5 @@
 library(rsconnect)
 
-# test version ----
-
 deployApp(account = 'psrcwa',
-          appName = 'development-rtp-dashboard',
-          appTitle = 'Development RTP Dashboard')
-
-# official version ----
-
-# deployApp(account = 'psrcwa',
-#           appName = 'rtp_dashboard',
-#           appTitle = 'RTP Dashboard')
+          appName = 'acs-commute-modes',
+          appTitle = 'PSRC Commute Modes')
